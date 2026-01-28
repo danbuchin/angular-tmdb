9fd4a8835b0721727d3f35413c7ad0ab583e8d3e
danbuchin <buchindaniil6@gmail.com>
2026-01-28 10:03:32 +0200

    created a component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MovieComponent } from './components/movie/movie.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UpperCasePipe],
+  imports: [MovieComponent, RouterOutlet, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
