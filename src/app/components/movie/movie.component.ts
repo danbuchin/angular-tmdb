625edca2650e1d036002477bcab1f51097a5a59a
danbuchin <buchindaniil6@gmail.com>
2026-01-28 16:27:42 +0200

    toggle details, decimal pipe

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,10 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { IMovie } from '../../models/movie';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
+  imports: [DecimalPipe],
 })
 export class MovieComponent {
   @Input() movie: IMovie;
+
+  details = false;
 }
