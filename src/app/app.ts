2f8b35e5206a199d27f4517e2d06f8e9f8d8cde6
danbuchin <buchindaniil6@gmail.com>
2026-01-29 12:04:50 +0200

    async pipe

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,37 +1,28 @@
-import { UpperCasePipe } from '@angular/common';
+import { AsyncPipe, UpperCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 import { MovieComponent } from './components/movie/movie.component';
 import { movies as data } from './data/movies';
-import { APIResponse, IMovie } from './models/movie';
+import { APIResponse } from './models/movie';
 import { MoviesService } from './services/movies.service';
 
 @Component({
   selector: 'app-root',
-  imports: [MovieComponent, RouterOutlet, UpperCasePipe],
+  imports: [AsyncPipe, MovieComponent, RouterOutlet, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App implements OnInit {
   protected title = 'angular-tmdb';
-  movies: IMovie[] = [];
+  movies$: Observable<APIResponse>;
   loading = false;
 
   constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
     this.loading = true;
-    try {
-      this.moviesService
-        .getByName('mission impossible')
-        .subscribe((movielist) => {
-          this.movies = movielist.results;
-        });
-    } catch (e) {
-      // If request fails, use static search results for 'kingsman'
-      this.movies = data;
-    } finally {
-      this.loading = false;
-    }
+    this.movies$ = this.moviesService.getByName('mission impossible');
+    this.loading = false;
   }
 }
